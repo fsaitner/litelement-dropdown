4cd74558bf61f9559e3bf59b602880dc6ac8b417
daPuce <florian.saitner@gmail.com>
2024-06-05 12:55:42 +0200

    Add fs-menu-item element

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import './style.scss';
 import './components/icon/fs-icon';
 import './components/button/fs-button';
+import './components/menu-item/fs-menu-item';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>LitElement Dropdown Component</h1>
     <fs-icon icon="chevron-down"></fs-icon>
     <fs-button caret>Dropdown</fs-button>
+    <fs-menu-item>Menu item 1</fs-menu-item>
+    <fs-menu-item active>Menu item 2</fs-menu-item>
+    <fs-menu-item>Menu item 3</fs-menu-item>
   </div>
 `;
