42cc0ff66c5dbe2cb13d196121e7583738c1996b
daPuce <florian.saitner@gmail.com>
2024-06-05 12:06:20 +0200

    Add fs-button element

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import './style.scss';
 import './components/icon/fs-icon';
+import './components/button/fs-button';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>LitElement Dropdown Component</h1>
     <fs-icon icon="chevron-down"></fs-icon>
+    <fs-button caret>Dropdown</fs-button>
   </div>
 `;
