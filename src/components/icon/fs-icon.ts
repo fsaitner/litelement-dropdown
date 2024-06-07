4f4cca3cf4d237f3da8433250def3a67940e664a
daPuce <florian.saitner@gmail.com>
2024-06-07 08:29:34 +0200

    Change color of error icon

--- src/components/icon/fs-icon.ts
+++ src/components/icon/fs-icon.ts
@@ -58,7 +58,7 @@ export class fsIcon extends LitElement {
         /**
          * Return "error" SVG icon to indicate missing icon attribute on element.
          */
-        return html`<svg width="16" height="16" fill="#dc3545" viewBox="0 0 16 16">
+        return html`<svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path
             d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"
           />
